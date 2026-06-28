4a8fbaa04c140056c2f1902bbfa855cccf061a48
yulchanshin <yulchanshin@gmail.com>
2026-06-27 19:08:35 -0700

    test(cpp-worker): add transposed matmul benchmark for TSK-103

--- cpp-worker/benchmarks/benchmark_main.cpp
+++ cpp-worker/benchmarks/benchmark_main.cpp
@@ -32,17 +32,39 @@ BM_Naive_Matrix_Multiplication_Accumulator(benchmark::State& state) {
     }
 }
 
+static void BM_Matrix_Multiplication_Transposed(benchmark::State& state) {
+    std::size_t N = static_cast<std::size_t>(state.range(0));
+    std::vector<float> ones(N * N, 1.0f);
+    std::vector<float> ones2(N * N, 1.0f);
+
+    const Matrix A(N, N, std::move(ones));
+    const Matrix B(N, N, std::move(ones2));
+
+    for (const auto& _ : state) {
+        Matrix C = Matrix::multiply_matrix_transpose(A, B);
+        benchmark::DoNotOptimize(C);
+    }
+}
+
+
 BENCHMARK(BM_Naive_Matrix_Multiplication)
     ->Arg(64)
     ->Arg(128)
     ->Arg(256)
     ->Arg(512)
     ->Arg(1024);
+/*
 BENCHMARK(BM_Naive_Matrix_Multiplication_Accumulator)
     ->Arg(64)
     ->Arg(128)
     ->Arg(256)
     ->Arg(512)
     ->Arg(1024);
-
+*/
+BENCHMARK(BM_Matrix_Multiplication_Transposed)
+    ->Arg(64)
+    ->Arg(128)
+    ->Arg(256)
+    ->Arg(512)
+    ->Arg(1024);
 BENCHMARK_MAIN();
